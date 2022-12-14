97938d08bd16613f3c64e94b391ccd0dd7b84de6
ludovicjj <jahanlud@gmail.com>
2022-12-14 14:33:38 +0100

    Connect to database

---
+++ config/db.js
@@ -0,0 +1,8 @@
+const { mongoose } = require('mongoose');
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.r5ay06b.mongodb.net/mern`
+
+mongoose
+    .set('strictQuery', true)
+    .connect(url)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Failed to connect to MongoDB', err))
