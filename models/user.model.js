7556b5e7f85f6861bb3c6b1917262387f288049a
ludovicjj <jahanlud@gmail.com>
2022-12-15 11:41:49 +0100

    Define a pre hook to hash password using bcrypt on save

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt")
 
 const userSchema = new mongoose.Schema(
     {
@@ -24,6 +25,10 @@ const userSchema = new mongoose.Schema(
             maxLength: 1024,
             minLength: 6
         },
+        picture: {
+            type: String,
+            default: "./uploads/profil/random-user.png",
+        },
         bio: {
             type: String,
             maxLength: 1024
@@ -42,6 +47,12 @@ const userSchema = new mongoose.Schema(
         timestamps: true
     }
 );
+
+userSchema.pre("save", async function(next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
 
 const UserModel = mongoose.model('user', userSchema)
 module.exports = UserModel;
